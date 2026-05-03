c8e7d42a8ee77f244adbac8918b50a7e8657bade
Stuart Marsden <stuartmarsden@192.168.0.32>
2026-05-03 17:45:33 +0700

    chore: add Ahrefs Analytics to DeferredScripts

--- src/components/DeferredScripts.tsx
+++ src/components/DeferredScripts.tsx
@@ -58,6 +58,22 @@ export default function DeferredScripts() {
           gtag('config', 'G-X748S6W9M3');
         `}
       </Script>
+      {/* Microsoft Clarity */}
+      <Script id="microsoft-clarity" strategy="afterInteractive">
+        {`
+          (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "wl8syup6x3");
+        `}
+      </Script>
+      {/* Ahrefs Analytics */}
+      <Script 
+        src="https://analytics.ahrefs.com/analytics.js" 
+        data-key="qbKkKztWQFR3pbkK0jP78Q" 
+        strategy="afterInteractive"
+      />
     </>
   );
 }
